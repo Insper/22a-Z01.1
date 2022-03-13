e5cc07dbebcbbfed7189e17cbf24180232d3210e
Andre Brito <felipe.brito016@gmail.com>
2022-03-13 19:35:40 -0300

    finished or8Way

--- Projetos/B-LogicaCombinacional/src/Or8Way.vhd
+++ Projetos/B-LogicaCombinacional/src/Or8Way.vhd
@@ -15,11 +15,7 @@ entity Or8Way is
 end entity;
 
 architecture arch of Or8Way is
-	when a = '1' or b = '1' or c = '1' or d = '1' or e = '1' or f = '1' or g = '1' or h = '1'
-		then q <= '1';
-	when others
-		then q <= '0';
+	
 begin
-
-
+	q <= (a or b or c or d or e or f or g or h);
 end architecture;
--- Projetos/B-LogicaCombinacional/src/TopLevel.vhd
+++ Projetos/B-LogicaCombinacional/src/TopLevel.vhd
@@ -21,7 +21,7 @@ entity TopLevel is
 	port(
 		CLOCK_50 : in  std_logic;
 		SW       : in  std_logic_vector(9 downto 0);
-]		LEDR     : out std_logic_vector(9 downto 0);
+		LEDR     : out std_logic_vector(9 downto 0);
 		HEX0		: out std_logic_vector(6 downto 0);
 		HEX1 : out std_logic_vector(6 downto 0);
 		HEX2 : out std_logic_vector(6 downto 0);
