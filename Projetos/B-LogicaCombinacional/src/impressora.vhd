07c4c78219c39ca8b3bccdef85767f0c67d4c287
Andre Brito <felipe.brito016@gmail.com>
2022-03-14 23:29:05 -0300

    impressora implemented

--- Projetos/B-LogicaCombinacional/src/impressora.vhd
+++ Projetos/B-LogicaCombinacional/src/impressora.vhd
@@ -10,6 +10,10 @@ end entity;
 architecture arch of impressora is
 
 begin
-
+    x <= (not SW1 and not SW2) or 
+    (not SW1 and not SW3) or 
+    (not SW2 and not SW3) or 
+    (not SW2 and not SW4) or 
+    (not SW3 and not SW4);
 
 end architecture;
