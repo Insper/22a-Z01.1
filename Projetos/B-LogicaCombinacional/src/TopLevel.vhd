38c0521df09290709f8eacffe49689834991d14d
Enricco Gemha <81189101+G3mha@users.noreply.github.com>
2022-03-16 17:54:03 -0300

    Update TopLevel.vhd

--- Projetos/B-LogicaCombinacional/src/TopLevel.vhd
+++ Projetos/B-LogicaCombinacional/src/TopLevel.vhd
@@ -20,8 +20,10 @@ use work.all;
 entity TopLevel is
 	port(
 		CLOCK_50 : in  std_logic;
-		SW       : in  std_logic_vector(3 downto 0);
-		HEX0     : out std_logic_vector(6 downto 0)
+		SW       : in  std_logic_vector(9 downto 0);
+		HEX0     : out std_logic_vector(6 downto 0);
+		HEX1     : out std_logic_vector(6 downto 0);
+		HEX2     : out std_logic_vector(6 downto 0)
 );
 end entity;
 
@@ -35,6 +37,12 @@ architecture rtl of TopLevel is
 			bcd : in  STD_LOGIC_VECTOR(3 downto 0);
 			leds : out STD_LOGIC_VECTOR(6 downto 0));
 	end component;
+	
+	component sevenSeglinha is
+		port (
+			bcdlinha : in  STD_LOGIC_VECTOR(1 downto 0);
+			ledslinha : out STD_LOGIC_VECTOR(6 downto 0));
+	end component;
 --------------
 -- signals
 --------------
@@ -43,10 +51,16 @@ architecture rtl of TopLevel is
 -- implementacao
 ---------------
 begin
-	u1 : sevenSeg port map(
-		bcd => SW,
-		leds => HEX0
+u1 : sevenSeg port map(
+	bcd => SW(3 downto 0),
+	leds => HEX0
 );
-
+u2 : sevenSeg port map(
+	bcd => SW(7 downto 4),
+	leds => HEX1
+);
+u3 : sevenSeglinha port map(
+	bcdlinha => SW(9 downto 8),
+	ledslinha => HEX2
+);
 end rtl;
-
