a271598e30dafb3fe9e755f6f724e80d3d659393
Arthur-Barreto <arthurbarreto2905@gmail.com>
2022-03-16 07:05:17 -0300

    detector de moedas e impressora rodando

--- Projetos/B-LogicaCombinacional/src/detectorDeMoedas.vhd
+++ Projetos/B-LogicaCombinacional/src/detectorDeMoedas.vhd
@@ -3,12 +3,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity detectorDeMoedas is
 	port (
-    Q,D,N : in  STD_LOGIC;
+    Q,D,N     : in  STD_LOGIC;
     cents     : out STD_LOGIC_VECTOR(4 downto 0));
 end entity;
 
 architecture arch of detectorDeMoedas is
 
 begin
+    cents <= "00101" when ((not Q) and (not D) and N) else
+             "01010" when ((not Q) and D and (not N)) else
+             "11001" when (Q and (not D) and (not N)) else
+             "00000"; 
 
 end architecture;
