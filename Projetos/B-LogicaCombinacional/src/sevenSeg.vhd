164bf3908568066b8848dab96f97896031d9ad0c
borg <paulohenriquemoreirafalcao@gmail.com>
2022-03-16 19:32:10 -0300

    7seg

--- Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
+++ Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
@@ -10,4 +10,16 @@ end entity;
 architecture arch of sevenSeg is
 begin
 
+leds <= "1000000" when bcd = "0000" else 
+     	"0110000" when bcd = "0001" else 
+     	"0010010" when bcd = "0010" else 
+		"0110000" when bcd = "0011" else
+     	"1001100" when bcd = "0100" else
+		"0100100" when bcd = "0101" else
+		"0100000" when bcd = "0110" else
+		"1111000" when bcd = "0111" else
+		"0000000" when bcd = "1000" else
+		"0000100" when bcd = "1001" else
+		"1111111";
+
 end architecture;
