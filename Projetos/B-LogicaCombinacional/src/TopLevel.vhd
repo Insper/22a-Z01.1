5d8e5dee73a759b362b3387fb45d36d2bc9a4505
Andre Brito <felipe.brito016@gmail.com>
2022-03-16 14:45:31 -0300

    conceito a supimpa

--- Projetos/B-LogicaCombinacional/src/TopLevel.vhd
+++ Projetos/B-LogicaCombinacional/src/TopLevel.vhd
@@ -47,8 +47,14 @@ architecture rtl of TopLevel is
 			bcd => SW(3 downto 0),
 			leds => HEX0);
 		
-		HEX1 <= "1111111";
-		HEX2 <= "1111111";
+		u2: sevenSeg port map (
+			bcd => SW(7 downto 4),
+			leds => HEX1);
+		
+		u3: sevenSeg port map (
+			bcd => "00" & SW(9 downto 8),
+			leds => HEX2);
+		
 		HEX3 <= "1111111";
 		HEX4 <= "1111111";
 		HEX5 <= "1111111";
