57e369b0bd193f18b8ee8c1478e50d05020c3fd4
Felipe Trintim <felipemt3@al.insper.edu.br>
2022-03-16 13:51:52 -0300

    not16

---
+++ Projetos/B-LogicaCombinacional/src/Not16.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Not16 is
+	port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+end entity;
+
+architecture arch of Not16 is
+begin
+
+q <= a not b;
+
+end architecture;
