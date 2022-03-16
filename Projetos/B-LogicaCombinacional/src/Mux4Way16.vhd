cbb6cda5a7ba659e0ae2656dd0a4ce95a1cb95b5
Caio Tieri <caranhatieri@gmail.com>
2022-03-16 14:32:13 -0300

    Caio Tieri Issues

---
+++ Projetos/B-LogicaCombinacional/src/Mux4Way16.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Mux4Way16 is
+	port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			b:   in  STD_LOGIC_VECTOR(15 downto 0);
+			c:   in  STD_LOGIC_VECTOR(15 downto 0);
+			d:   in  STD_LOGIC_VECTOR(15 downto 0);
+			sel: in  STD_LOGIC_VECTOR(1 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+end entity;
+
+architecture arch of Mux4Way16 is
+begin
+
+q <= a when sel = "00" else
+	b when sel = "01" else
+	c when sel = "10" else
+	d when sel = "11" else "0000000000000000";
+
+end architecture;
