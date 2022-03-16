9999a46e134146038a4467cbefa3a74250071062
borg <paulohenriquemoreirafalcao@gmail.com>
2022-03-16 15:23:41 -0300

    Detector de moedas

--- Projetos/B-LogicaCombinacional/src/detectorDeMoedas.vhd
+++ Projetos/B-LogicaCombinacional/src/detectorDeMoedas.vhd
@@ -10,5 +10,8 @@ end entity;
 architecture arch of detectorDeMoedas is
 
 begin
-
+cents <= "11001" when Q = '1' else 
+         "01010" when D = '1' else 
+         "00101" when N = '1' else 
+         "00000";
 end architecture;
