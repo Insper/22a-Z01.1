0b18d9fa8bfc6fb8fff96995da7de6c0c1d7faed
Caio Tieri <caranhatieri@gmail.com>
2022-03-16 15:01:38 -0300

    detector de medas

---
+++ Projetos/B-LogicaCombinacional/src/detectorDeMoedas.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity detectorDeMoedas is
+	port (
+    Q,D,N : in  STD_LOGIC;
+    cents     : out STD_LOGIC_VECTOR(4 downto 0));
+end entity;
+
+architecture arch of detectorDeMoedas is
+
+begin  
+    cents <= "11001" when Q else
+    "01010" when D else
+    "00101" when N else "00000";
+
+end architecture;
