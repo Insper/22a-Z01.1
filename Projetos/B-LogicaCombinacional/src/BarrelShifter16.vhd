7d315c35bddcf67b88b9ae727cd02d23d120da32
Leonardo Andrade <leofmandrade@gmail.com>
2022-03-17 10:32:42 -0300

    barrel shifter feito

--- Projetos/B-LogicaCombinacional/src/BarrelShifter16.vhd
+++ Projetos/B-LogicaCombinacional/src/BarrelShifter16.vhd
@@ -6,12 +6,12 @@ entity BarrelShifter16 is
 	port ( 
 			a:    in  STD_LOGIC_VECTOR(15 downto 0);   -- input vector
 			dir:  in  std_logic;                       -- 0=>left 1=>right
-			size: in  std_logic_vector(3 downto 0);    -- shift amount
+			size: in  std_logic_vector(2 downto 0);    -- shift amount
 			q:    out STD_LOGIC_VECTOR(15 downto 0));  -- output vector (shifted)
 end entity;
 
 architecture rtl of BarrelShifter16 is
 begin
-	q <= STD_LOGIC_VECTOR(shift_left(unsigned(a), to_integer(unsigned(size)))) when dir = '0' else 
+q <= STD_LOGIC_VECTOR(shift_left(unsigned(a), to_integer(unsigned(size)))) when dir = '0' else 
 	STD_LOGIC_VECTOR(shift_right(unsigned(a), to_integer(unsigned(size)))); 
 end architecture;
