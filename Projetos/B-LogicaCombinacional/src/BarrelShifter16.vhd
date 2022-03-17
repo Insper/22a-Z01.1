59a350cac2a54779d27c4df14e9417a512b81e75
Ariel Leventhal <arietl@al.insper.edu.br>
2022-03-17 18:39:00 -0300

    fixes #17, fixes #28

--- Projetos/B-LogicaCombinacional/src/BarrelShifter16.vhd
+++ Projetos/B-LogicaCombinacional/src/BarrelShifter16.vhd
@@ -1,17 +1,22 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use ieee.numeric_std.all;
 entity BarrelShifter16 is
 	port ( 
 			a:    in  STD_LOGIC_VECTOR(15 downto 0);   -- input vector
 			dir:  in  std_logic;                       -- 0=>left 1=>right
-			size: in  std_logic_vector(3 downto 0);    -- shift amount
+			size: in  std_logic_vector(2 downto 0);    -- shift amount
 			q:    out STD_LOGIC_VECTOR(15 downto 0));  -- output vector (shifted)
 end entity;
 
 architecture rtl of BarrelShifter16 is
+signal b : unsigned(15 downto 0);
+signal sizeb : unsigned(2 downto 0);
 begin
-
+	b <= unsigned(a);
+	sizeb <= unsigned(size);
+	q <= STD_LOGIC_VECTOR(shift_left(b,to_integer(sizeb))) when dir = '0' else STD_LOGIC_VECTOR(shift_right(b,to_integer(sizeb))) ;
 
 end architecture;
 
--- Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
+++ Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
@@ -8,6 +8,26 @@ entity sevenSeg is
 end entity;
 
 architecture arch of sevenSeg is
+signal led: STD_LOGIC_VECTOR(6 downto 0);
 begin
+	with bcd select
 
+		led <= "0111111" when "0000",
+				"0000110" when "0001",
+				"0000110" when "0010",
+				"1001111" when "0011",
+				"1100110" when "0100",
+				"1101101" when "0101",
+				"1111101" when "0110",
+				"0000111" when "0111",
+				"1111111" when "1000",
+				"1101111" when "1001",
+				"1110111" when "1010",
+				"1111100" when "1011",
+				"0111001" when "1100",
+				"1011110" when "1101",
+				"1111001" when "1110",
+				"1110001" when others;
+		leds <= not led;
+				
 end architecture;
