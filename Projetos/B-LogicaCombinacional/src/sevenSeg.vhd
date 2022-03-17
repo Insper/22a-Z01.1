2b43fddadd5c45dda6ddf58cb8efbd04f8e6cf18
Felipe Trintim <felipemt3@al.insper.edu.br>
2022-03-17 14:06:38 -0300

    seven

--- Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
+++ Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
@@ -9,5 +9,14 @@ end entity;
 
 architecture arch of sevenSeg is
 begin
-
+leds <= "1000000" when bcd = "0000" else
+	"1111001" when bcd = "0001" else
+	"0100100" when bcd = "0010" else
+	"0110000" when bcd = "0011" else
+	"0011001" when bcd = "0100" else
+	"0010010" when bcd = "0101" else
+	"0000010" when bcd = "0110" else
+	"1111000" when bcd = "0111" else
+	"0000000" when bcd = "1000" else
+	"0011000" when bcd = "1001" else "0000000";
 end architecture;
