3c5e3355bfa92be4c749db29ec34e4683e3cd77b
Arthur-Barreto <arthurbarreto2905@gmail.com>
2022-03-30 15:04:56 -0300

    register8 done

--- Projetos/D-LogicaSequencial/src/Register8.vhd
+++ Projetos/D-LogicaSequencial/src/Register8.vhd
@@ -26,7 +26,60 @@ architecture arch of Register8 is
 	end component;
 
 begin
+	bit0: BinaryDigit port map(
+		clock => clock,
+		input => input(0),
+		load  => load,
+		output => output(0)
+	);
 	
+	bit1: BinaryDigit port map(
+		clock => clock,
+		input => input(1),
+		load  => load,
+		output => output(1)
+	);
 
+	bit2: BinaryDigit port map(
+		clock => clock,
+		input => input(2),
+		load  => load,
+		output => output(2)
+	);
 
+	bit3: BinaryDigit port map(
+		clock => clock,
+		input => input(3),
+		load  => load,
+		output => output(3)
+	);
+
+	bit4: BinaryDigit port map(
+		clock => clock,
+		input => input(4),
+		load  => load,
+		output => output(4)
+	);
+
+	bit5: BinaryDigit port map(
+		clock => clock,
+		input => input(5),
+		load  => load,
+		output => output(5)
+	);
+
+	bit6: BinaryDigit port map(
+		clock => clock,
+		input => input(6),
+		load  => load,
+		output => output(6)
+	);
+
+	bit7: BinaryDigit port map(
+		clock => clock,
+		input => input(7),
+		load  => load,
+		output => output(7)
+	);
+	
 end architecture;
