90daa95834ef104422fb7bcb20ee3f8713d62bbd
Andre Brito <felipe.brito016@gmail.com>
2022-04-11 21:29:22 -0300

    Ram8 and FlipFlopT fix

--- Projetos/D-LogicaSequencial/src/TopLevel.vhd
+++ Projetos/D-LogicaSequencial/src/TopLevel.vhd
@@ -1,67 +1,146 @@
---
--- Elementos de Sistemas - Aula 5 - Logica Combinacional
--- Rafael . Corsi @ insper . edu . br 
---
--- Arquivo exemplo para acionar os LEDs e ler os bottoes
--- da placa DE0-CV utilizada no curso de elementos de 
--- sistemas do 3s da eng. da computacao
+-- Elementos de Sistemas
+-- by Luciano Soares
+-- Ram8.vhd
 
-----------------------------
--- Bibliotecas ieee       --
-----------------------------
-library ieee;
+Library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.all;
 
-----------------------------
--- Entrada e saidas do bloco
-----------------------------
 entity TopLevel is
 	port(
-		clock:   in STD_LOGIC;
-		input:   in STD_LOGIC_VECTOR(15 downto 0);
-		load:    in STD_LOGIC;
-		output: out STD_LOGIC_VECTOR(15 downto 0)
+		clock:   in  STD_LOGIC;
+		input:   in  STD_LOGIC_VECTOR(15 downto 0);
+		load:    in  STD_LOGIC;
+		address: in  STD_LOGIC_VECTOR(2 downto 0);
+		output:  out STD_LOGIC_VECTOR(15 downto 0)
 	);
 end entity;
 
-----------------------------
--- Implementacao do bloco -- 
-----------------------------
-architecture rtl of TopLevel is
+architecture arch of TopLevel is
 
+	component Register16 is
+		port(
+			clock:   in  STD_LOGIC;
+			input:   in  STD_LOGIC_VECTOR(15 downto 0);
+			load:    in  STD_LOGIC;
+			output:  out STD_LOGIC_VECTOR(15 downto 0)
+		);
+	end component;
 
-component Register8 is
-	port(
-		clock:   in STD_LOGIC;
-		input:   in STD_LOGIC_VECTOR(7 downto 0);
-		load:    in STD_LOGIC;
-		output: out STD_LOGIC_VECTOR(7 downto 0)
-	);
-end component;
-signal input_reg: STD_LOGIC_VECTOR(15 downto 0);
-signal output_reg: STD_LOGIC_VECTOR(15 downto 0);
-signal load_reg: STD_LOGIC;
+	component Mux8Way16 is
+		port (
+				a:   in  STD_LOGIC_VECTOR(15 downto 0);
+				b:   in  STD_LOGIC_VECTOR(15 downto 0);
+				c:   in  STD_LOGIC_VECTOR(15 downto 0);
+				d:   in  STD_LOGIC_VECTOR(15 downto 0);
+				e:   in  STD_LOGIC_VECTOR(15 downto 0);
+				f:   in  STD_LOGIC_VECTOR(15 downto 0);
+				g:   in  STD_LOGIC_VECTOR(15 downto 0);
+				h:   in  STD_LOGIC_VECTOR(15 downto 0);
+				sel: in  STD_LOGIC_VECTOR(2 downto 0);
+				q:   out STD_LOGIC_VECTOR(15 downto 0));
+	end component;
 
----------------
--- implementacao
----------------
+	component DMux8Way is
+		port (
+			a:   in  STD_LOGIC;
+			sel: in  STD_LOGIC_VECTOR(2 downto 0);
+			q0:  out STD_LOGIC;
+			q1:  out STD_LOGIC;
+			q2:  out STD_LOGIC;
+			q3:  out STD_LOGIC;
+			q4:  out STD_LOGIC;
+			q5:  out STD_LOGIC;
+			q6:  out STD_LOGIC;
+			q7:  out STD_LOGIC
+		);
+	end component;
+
+	signal load0, load1, load2, load3, load4, load5, load6, load7 : STD_LOGIC;
+	signal output0, output1, output2, output3, output4, output5, output6, output7 : STD_LOGIC_VECTOR(15 downto 0);
+
 begin
-	b0 : Register8 port map (
-		clock => clock,
-		input => input_reg(7 downto 0),
-		load => load_reg,
-		output => output_reg(7 downto 0)
-	);
-	b1 : Register8 port map (
-		clock => clock,
-		input => input_reg(15 downto 8),
-		load => load_reg,
-		output => output_reg(15 downto 8)
-	);
-	output<=output_reg;
-	input_reg<=input;
-	load_reg<=load;
 
-end rtl;
+	dMuxPm: DMux8Way port map(
+		load,
+		address,
+		load0,
+		load1,
+		load2,
+		load3,
+		load4,
+		load5,
+		load6,
+		load7
+	);
+
+	reg0Pm: Register16 port map(
+		clock,
+		input,
+		load0,
+		output0
+	);
+
+	reg1Pm: Register16 port map(
+		clock,
+		input,
+		load1,
+		output1
+	);
+
+	reg2Pm: Register16 port map(
+		clock,
+		input,
+		load2,
+		output2
+	);
+
+	reg3Pm: Register16 port map(
+		clock,
+		input,
+		load3,
+		output3
+	);
+
+	reg4Pm: Register16 port map(
+		clock,
+		input,
+		load4,
+		output4
+	);
+
+	reg5Pm: Register16 port map(
+		clock,
+		input,
+		load5,
+		output5
+	);
+
+	reg6Pm: Register16 port map(
+		clock,
+		input,
+		load6,
+		output6
+	);
+
+	reg7Pm: Register16 port map(
+		clock,
+		input,
+		load7,
+		output7
+	);
+
+	mux8Pm: Mux8Way16 port map(
+		output0,
+		output1,
+		output2,
+		output3,
+		output4,
+		output5,
+		output6,
+		output7,
+		address,
+		output
+	);
+
+
+end architecture;
