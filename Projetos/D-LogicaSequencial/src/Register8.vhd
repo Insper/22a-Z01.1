fe81c54725b4e4dc4f98bad96157a8fd3d97562f
EnzoLuidge <enzolpsb@al.insper.edu.br>
2022-04-11 21:06:05 -0300

    Registers8e16

--- Projetos/D-LogicaSequencial/src/Register8.vhd
+++ Projetos/D-LogicaSequencial/src/Register8.vhd
@@ -28,4 +28,78 @@ architecture arch of Register8 is
 begin
 
 
+
+	-- Agora com o componente 'criado'
+	-- podemos utilizar no nosso projeto
+	--
+	--  podemos dar um 'nome' à instancia do componente
+	--  |
+	--  |          nome do componente a ser usado
+	--  |          | 
+	--  v          v
+	BinaryDigit0 : BinaryDigit port map(
+		clock  => clock,
+		input  => input(0),
+		load   => load,
+		output => output(0)
+	);
+
+	BinaryDigit1 : BinaryDigit port map(
+		clock  => clock,
+		input  => input(1),
+		load   => load,
+		output => output(1)
+
+	);
+
+	BinaryDigit2 : BinaryDigit port map(
+		clock  => clock,
+		input  => input(2),
+		load   => load,
+		output => output(2)
+ 
+	);
+
+	BinaryDigit3 : BinaryDigit port map(
+		clock  => clock,
+		input  => input(3),
+		load   => load,
+		output => output(3)
+ 
+	);
+
+	BinaryDigit4 : BinaryDigit port map(
+		clock  => clock,
+		input  => input(4),
+		load   => load,
+		output => output(4)
+ 
+	);
+
+	BinaryDigit5 : BinaryDigit port map(
+		clock  => clock,
+		input  => input(5),
+		load   => load,
+		output => output(5)
+ 
+	);
+
+	BinaryDigit6 : BinaryDigit port map(
+		clock  => clock,
+		input  => input(6),
+		load   => load,
+		output => output(6)
+ 
+	);
+
+	BinaryDigit7 : BinaryDigit port map(
+		clock  => clock,
+		input  => input(7),
+		load   => load,
+		output => output(7)
+ 
+	);
+
+
+
 end architecture;
