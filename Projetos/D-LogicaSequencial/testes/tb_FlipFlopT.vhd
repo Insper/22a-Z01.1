73566238b6288a27d6e1adb2d34378ae097c77a6
Pedro2712 <72358343+Pedro2712@users.noreply.github.com>
2022-04-13 16:27:40 -0300

    Testes FlipFlopT feito

--- Projetos/D-LogicaSequencial/testes/tb_FlipFlopT.vhd
+++ Projetos/D-LogicaSequencial/testes/tb_FlipFlopT.vhd
@@ -17,8 +17,8 @@ architecture tb of tb_FlipFlopT is
     port(
       clock:  in std_logic;
       t:      in std_logic;
-      q:      out std_logic:= '0';
-      notq:   out std_logic:= '1'
+      q:      inout std_logic:= '0';
+      notq:   inout std_logic:= '1'
       );
 	end component;
 
@@ -35,12 +35,37 @@ begin
   begin
     test_runner_setup(runner, runner_cfg);
 
-    -- IMPLEMENTE AQUI!
-    wait until clk'event and clk='0';
+  -- Teste 0
+    t <= '0';
+  wait until clk'event and clk='0';
+    assert(q = '0' and notq = '1')  report "Falha em teste: 0" severity error;
 
-    -- finish
-    wait until clk'event and clk='0';
-    test_runner_cleanup(runner); -- Simulation ends here
+  -- Teste 1
+    t <= '1';
+  wait until clk'event and clk='0';
+    assert(q = '1' and notq = '0')  report "Falha em teste: 1" severity error;
+
+  -- Teste 2
+    t <= '1';
+  wait until clk'event and clk='0';
+    assert(q = '0' and notq = '1')  report "Falha em teste: 2" severity error;
+  
+  -- Teste 3
+    t <= '1';
+  wait until clk'event and clk='0';
+    assert(q = '1' and notq = '0')  report "Falha em teste: 3" severity error;
+  
+  -- Teste 4
+    t <= '0';
+  wait until clk'event and clk='0';
+    assert(q = '1' and notq = '0')  report "Falha em teste: 4" severity error;
+    
+  -- Teste 5
+    t <= '0';
+  wait until clk'event and clk='0';
+    assert(q = '1' and notq = '0')  report "Falha em teste: 5" severity error;
+
+  test_runner_cleanup(runner); -- Simulation ends here
 
 	wait;
   end process;
