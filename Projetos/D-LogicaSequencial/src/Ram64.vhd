6ebb0c488003896965def5793573dbc863b03129
pcliquet <pedroca8@al.insper.edu.br>
2022-04-13 14:47:46 -0300

    Ram 64

--- Projetos/D-LogicaSequencial/src/Ram64.vhd
+++ Projetos/D-LogicaSequencial/src/Ram64.vhd
@@ -60,5 +60,77 @@ architecture arch of Ram64 is
 
 begin
 
+	demux: DMux8Way port map (
+		a => load,
+		sel => address(2 downto 0),
+		q0 => load0,
+		q1 => load1,
+		q2 => load2,
+		q3 => load3,
+		q4 => load4,
+		q5 => load5,
+		q6 => load6,
+		q7 => load7
+	);
+	mux: Mux8Way16 port map (
+		a => output0,
+		b => output1,
+		c => output2,
+		d => output3,
+		e => output4,
+		f => output5,
+		g => output6,
+		h => output7,
+		sel => address(2 downto 0),
+		q => output
+	);
+	
+	r1: Ram8 port map(clock => clock, 
+		input => input, 
+		load => load0, 
+		address => address(2 downto 0), 
+		output => output0);
+
+	r2: Ram8 port map(clock => clock, 
+		input => input, 
+		load => load1, 
+		address => address(2 downto 0), 
+		output => output1);
+
+	r3: Ram8 port map(clock => clock, 
+		input => input, 
+		load => load2, 
+		address => address(2 downto 0), 
+		output => output2);
+
+	r4: Ram8 port map(clock => clock, 
+		input => input, 
+		load => load3, 
+		address => address(2 downto 0), 
+		output => output3);
+
+	r5: Ram8 port map(clock => clock, 
+		input => input, load => load4, 
+		address => address(2 downto 0), 
+		output => output4);
+
+	r6: Ram8 port map(clock => clock, 
+		input => input, 
+		load => load5, 
+		address => address(2 downto 0), 
+		output => output5);
+
+	r7: Ram8 port map(clock => clock, 
+		input => input, 
+		load => load6, 
+		address => address(2 downto 0), 
+		output => output6);
+
+	r8: Ram8 port map(clock => clock, 
+		input => input, load => load7, 
+		address => address(2 downto 0), 
+		output => output7);
+		
+
 
 end architecture;
