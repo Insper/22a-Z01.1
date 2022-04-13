73e6bee74a3f359ef40aeb50f3366b98fb0cf08a
pedrohois <pedrohois@al.insper.edu.br>
2022-04-13 15:15:26 -0300

    Binary, Register32e 64 feitas

--- Projetos/D-LogicaSequencial/src/Register32.vhd
+++ Projetos/D-LogicaSequencial/src/Register32.vhd
@@ -26,6 +26,19 @@ architecture arch of Register32 is
 	end component;
 
 begin
+	P1_Resgister32 : Register16
+		port map (
+			clock  => clock,
+			input  => input(15 downto 0),
+			load   => load,
+			output => output(15 downto 0)
+		);
 
-
+	P2_Resgister64 : Register16
+		port map (
+			clock  => clock,
+			input  => input(31 downto 16),
+			load   => load,
+			output => output(31 downto 16)
+	);
 end architecture;
