a6580517a020f486b9b6ed8c58eb77cf35e88140
gustavoeso <gustavo.eso@outlook.com.br>
2022-04-13 18:05:30 -0300

    video e seven concertado

--- Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
+++ Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
@@ -11,7 +11,7 @@ architecture arch of sevenSeg is
 begin
 	leds <= "1000000" when bcd = "0000" else	
 		"1111001" when bcd = "0001" else
-		"0101000" when bcd = "0010" else
+		"0100100" when bcd = "0010" else
 		"0110000" when bcd = "0011" else
 		"0011001" when bcd = "0100" else
 		"0010010" when bcd = "0101" else
