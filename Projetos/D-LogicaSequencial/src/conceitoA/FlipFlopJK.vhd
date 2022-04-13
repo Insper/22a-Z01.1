088b0e385c5ca1756e62da248c7388e6670c4bf8
Pedro2712 <72358343+Pedro2712@users.noreply.github.com>
2022-04-13 16:25:08 -0300

    FlipFlopJK feito

--- Projetos/D-LogicaSequencial/src/conceitoA/FlipFlopJK.vhd
+++ Projetos/D-LogicaSequencial/src/conceitoA/FlipFlopJK.vhd
@@ -9,13 +9,35 @@ entity FlipFlopJK is
 		clock:  in std_logic;
 		J:      in std_logic;
 		K:      in std_logic;
-		q:      out std_logic:= '0';
-		notq:   out std_logic:= '1'
+		q:      inout std_logic:= '0';
+		notq:   inout std_logic:= '1'
 	);
 end entity;
 
 architecture arch of FlipFlopJK is
 
 begin
+
+	process(clock, J, K) begin
+		if (rising_edge(clock)) then
+			if (J = '0' and K = '0') then
+				q <= q;
+				notq <= notq;
+			
+			elsif (J = '0' and K = '1') then
+				q <= J;
+				notq <= K;
+			
+			elsif (J = '1' and K = '0') then
+				q <= J;
+				notq <= K;
+			
+			elsif (J = '1' and K = '1') then
+				q <= not(q);
+				notq <= not(notq);
+			
+			end if ;			
+		end if;
+  end process;
 
 end architecture;
