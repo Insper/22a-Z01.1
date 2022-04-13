ca619fcd82bb49779a02dd95444bc81178ae9d48
Pedro2712 <72358343+Pedro2712@users.noreply.github.com>
2022-04-13 16:26:29 -0300

    Counter Down feito

--- Projetos/D-LogicaSequencial/src/conceitoA/CounterDown.vhd
+++ Projetos/D-LogicaSequencial/src/conceitoA/CounterDown.vhd
@@ -13,6 +13,22 @@ end entity;
 
 architecture arch of CounterDown is
 
+component FlipFlopT is
+	port(
+		clock:  in std_logic;
+		t:      in std_logic;
+		q:      inout std_logic:= '0';
+		notq:   inout std_logic:= '1'
+	);
+end component;
+
+	signal sq : std_logic_vector(2 downto 0);
+
 begin
 
+	flip0 : FlipFlopT port map(clock => clock, t => '1', q => sq(0), notq => Open);
+	flip1 : FlipFlopT port map(clock => sq(0), t => '1', q => sq(1), notq => Open);
+	flip2 : FlipFlopT port map(clock => sq(1), t => '1', q => sq(2), notq => Open);
+
+	q <= sq;
 end architecture;
