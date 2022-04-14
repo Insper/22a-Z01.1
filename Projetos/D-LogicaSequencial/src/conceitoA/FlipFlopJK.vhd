9a4463a2c91d03c05e4b318ea75b8ca4f66cae29
Arthur-Barreto <arthurbarreto2905@gmail.com>
2022-04-14 18:46:08 -0300

    flipflops finalizados e rtl dos mesmo

--- Projetos/D-LogicaSequencial/src/conceitoA/FlipFlopJK.vhd
+++ Projetos/D-LogicaSequencial/src/conceitoA/FlipFlopJK.vhd
@@ -9,32 +9,29 @@ entity FlipFlopJK is
 		clock:  in std_logic;
 		J:      in std_logic;
 		K:      in std_logic;
-		q:      out std_logic:= '0';
-		notq:   out std_logic:= '1'
+		q:      buffer std_logic:= '0';
+		notq:   buffer std_logic:= '1'
 	);
 end entity;
 
 architecture arch of FlipFlopJK is
 
-signal q_conection, notq_conection : std_logic;
-
 begin
 
 	process (clock) begin
 		if (rising_edge(clock) and J = '0' and K ='0') then
-			q_conection <= q_conection;
-			notq_conection <= notq_conection;
+			q <= q;
+			notq <= notq;
 		elsif (rising_edge(clock) and J = '0' and K ='1') then
-			q_conection <= '0';
-			notq_conection <= '1';
+			q <= '0';
+			notq <= '1';
 		elsif (rising_edge(clock) and J = '1' and K ='0') then	
-			q_conection <= '1';
-			notq_conection <= '0';
+			q <= '1';
+			notq <= '0';
 		elsif (rising_edge(clock) and J = '1' and K ='1') then	
-			q_conection <= notq_conection;
-			notq_conection <= q_conection;
+			q <= notq;
+			notq <= q;
 		end if;
 	end process;
-	q <= q_conection;
-	notq <= notq_conection;
+
 end architecture;
--- Projetos/D-LogicaSequencial/src/conceitoA/FlipFlopT.vhd
+++ Projetos/D-LogicaSequencial/src/conceitoA/FlipFlopT.vhd
@@ -8,29 +8,25 @@ entity FlipFlopT is
 	port(
 		clock:  in std_logic;
 		t:      in std_logic;
-		q:      out std_logic:= '0';
-		notq:   out std_logic:= '1'
+		q:      buffer std_logic:= '0';
+		notq:   buffer std_logic:= '1'
 	);
 end entity;
 
 architecture arch of FlipFlopT is
 
-signal q_conection, notq_conection : std_logic;
 
 begin
 
 	process(clock) begin
 		if (rising_edge(CLOCK)) then
 			if (t = '0') then
-				q_conection <= q_conection;
-				notq_conection <= notq_conection;
+				q <= q;
+				notq <= notq;
 			elsif (t = '1') then
-				q_conection <= not(q_conection);
-				notq_conection <= not(notq_conection);
+				q <= not(q);
+				notq <= not(notq);
 			end if;
 		end if;
-	end process;
-	q <= q_conection;
-	notq <= notq_conection;
-
+  end process;
 end architecture;
