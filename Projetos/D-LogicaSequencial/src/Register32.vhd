60cee8bd5cd352b61959f48a9749a42b57e7ff50
Caio Bertolani Travain <81189640+caiotravain@users.noreply.github.com>
2022-04-14 17:05:59 -0300

    Update Register32.vhd

--- Projetos/D-LogicaSequencial/src/Register32.vhd
+++ Projetos/D-LogicaSequencial/src/Register32.vhd
@@ -27,5 +27,16 @@ architecture arch of Register32 is
 
 begin
 
-
+	S0 : Register16 port map (
+		clock => clock,
+		input=> input(15 downto 0),
+		load => load,
+		output => output(15 downto 0)
+	);
+	S1 : Register16 port map (
+		clock => clock,
+		input=> input(31 downto 16),
+		load => load,
+		output => output(31 downto 16)
+	);
 end architecture;
