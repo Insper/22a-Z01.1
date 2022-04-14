20062a2bbd743088ddfd17ce11b7ca0067a24e76
Caio Bertolani Travain <81189640+caiotravain@users.noreply.github.com>
2022-04-14 16:52:01 -0300

    Update Register16.vhd

--- Projetos/D-LogicaSequencial/src/Register16.vhd
+++ Projetos/D-LogicaSequencial/src/Register16.vhd
@@ -26,6 +26,18 @@ architecture arch of Register16 is
 	end component;
 
 begin
+S0 : Register8 port map (
+	clock => clock,
+	input=> input(7 downto 0),
+	load => load,
+	output => output(7 downto 0)
+);
+S1 : Register8 port map (
+	clock => clock,
+	input=> input(15 downto 8),
+	load => load,
+	output => output(15 downto 8)
+);
 
 
 end architecture;
