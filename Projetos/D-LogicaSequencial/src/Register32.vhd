e3315af16e94440cdf57c5bffc2d9bc61ae51722
DomRuanSuzano <dom.ruan.drs@gmail.com>
2022-04-14 19:20:48 -0300

    FlipFlopD e Reg32, mais descomentando os testes

--- Projetos/D-LogicaSequencial/src/Register32.vhd
+++ Projetos/D-LogicaSequencial/src/Register32.vhd
@@ -27,5 +27,7 @@ architecture arch of Register32 is
 
 begin
 
+	registro0: Register16 port map(clock, input(15 downto 0), load, output(15 downto 0));
+	registro1: Register16 port map(clock, input(31 downto 16), load, output(31 downto 16));
 
 end architecture;
