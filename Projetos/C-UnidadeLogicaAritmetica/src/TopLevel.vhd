a01dd6205c14f0608ac19d1f6504d5fe7b79d24c
pedrohois <pedrohois@al.insper.edu.br>
2022-04-20 13:48:22 -0300

    Corrigindo erro no C - Top level

--- Projetos/C-UnidadeLogicaAritmetica/src/TopLevel.vhd
+++ Projetos/C-UnidadeLogicaAritmetica/src/TopLevel.vhd
@@ -19,7 +19,7 @@ use ieee.numeric_std.all;
 entity TopLevel is
 	port(
 		SW      : in  std_logic_vector(9 downto 0);
-		LEDR    : out std_logic_vector(9 downto 0)
+		LEDR    : out std_logic_vector(9 downto 0);
 		HEX0     : out std_logic_vector(6 downto 0); -- 7seg0
     HEX1     : out std_logic_vector(6 downto 0); -- 7seg0
     HEX2     : out std_logic_vector(6 downto 0); -- 7seg0
