ef08f5f8250ac4c6919807d83c7081235b3b12a5
rafapaolino <paulohenriquemoreirafalcao@gmail.com>
2022-05-04 18:07:38 -0300

    ControlUnit

--- Projetos/F-Computador/src/ControlUnit.vhd
+++ Projetos/F-Computador/src/ControlUnit.vhd
@@ -12,16 +12,16 @@ use ieee.std_logic_1164.all;
 
 entity ControlUnit is
     port(
-		instruction                 : in STD_LOGIC_VECTOR(17 downto 0);  -- instrução para executar
-		zr,ng                       : in STD_LOGIC;                      -- valores zr(se zero) e
+                instruction                 : in STD_LOGIC_VECTOR(17 downto 0);  -- instrução para executar
+                zr,ng                       : in STD_LOGIC;                      -- valores zr(se zero) e
                                                                      -- ng (se negativo) da ALU
-		muxALUI_A                   : out STD_LOGIC;                     -- mux que seleciona entre
+                muxALUI_A                   : out STD_LOGIC;                     -- mux que seleciona entre
                                                                      -- instrução e ALU para reg. A
-		muxAM                       : out STD_LOGIC;                     -- mux que seleciona entre
+                muxAM                       : out STD_LOGIC;                     -- mux que seleciona entre
                                                                      -- reg. A e Mem. RAM para ALU
                                                                      -- A  e Mem. RAM para ALU
-		zx, nx, zy, ny, f, no       : out STD_LOGIC;                     -- sinais de controle da ALU
-		loadA, loadD, loadM, loadPC : out STD_LOGIC               -- sinais de load do reg. A,
+                zx, nx, zy, ny, f, no       : out STD_LOGIC;                     -- sinais de controle da ALU
+                loadA, loadD, loadM, loadPC : out STD_LOGIC               -- sinais de load do reg. A,
                                                                      -- reg. D, Mem. RAM e Program Counter
     );
 end entity;
@@ -29,6 +29,7 @@ end entity;
 architecture arch of ControlUnit is
 
 begin
+
     loadD <= instruction(17) and instruction(4);
     loadM <= instruction(17) and instruction(5);
     loadA <= not(instruction(17)) or instruction(3);
@@ -39,4 +40,13 @@ begin
     ny <= instruction(17) and instruction(9);
     f <= instruction(17) and instruction(8);
     no <= instruction(17) and instruction(7);
-end architecture;
+    muxAM <= instruction(17) and instruction(5);
+    loadPC <= ((instruction(17) and (not(instruction(2)) and not(instruction(1)) and instruction(0))) and (not(zr) and not(ng)))
+    or ((instruction(17) and (not(instruction(2)) and instruction(1) and not(instruction(0)))) and (zr))
+    or ((instruction(17) and (not(instruction(2)) and instruction(1) and instruction(0))) and (not(ng)))
+    or ((instruction(17) and (instruction(2) and not(instruction(1)) and not(instruction(0)))) and (not(zr) and (ng)))
+    or ((instruction(17) and (instruction(2) and not(instruction(1)) and instruction(0))) and  not(zr))
+    or ((instruction(17) and (instruction(2) and instruction(1) and not(instruction(0)))) and (ng))
+    or ((instruction(17) and (instruction(2) and instruction(1) and instruction(0))));
+
+end architecture; 
