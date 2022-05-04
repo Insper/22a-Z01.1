ee9d830eb4553c4204333b07c306931b13c1d315
PedroPauloMorenoCamargo <pedropmc1@al.insper.edu.br>
2022-05-04 15:12:25 -0300

    CPU_CONCEITO_C

--- Projetos/F-Computador/src/CPU.vhd
+++ Projetos/F-Computador/src/CPU.vhd
@@ -78,7 +78,7 @@ architecture arch of CPU is
 
   signal c_muxALUI_A: STD_LOGIC;
   signal c_muxAM: STD_LOGIC;
-  signal c_zx: STD_LOGIC;
+  signal c_zx: STD_LOGIC;x'
   signal c_nx: STD_LOGIC;
   signal c_zy: STD_LOGIC;
   signal c_ny: STD_LOGIC;
@@ -99,5 +99,82 @@ architecture arch of CPU is
   signal s_pcout: STD_LOGIC_VECTOR(15 downto 0);
 
 begin
+
+  ControlUnit: ControlUnit port map(
+    instruction => instruction,
+    zr => c_zr,
+    ng => c_ng,
+    muxALUI_A => c_muxALUI_A,
+    muxAM => c_muxAM,
+    zx => c_zx,
+    nx => c_nx,
+    zy => c_zy,
+    ny => c_ny,
+    f => c_f,
+    no => c_no,
+    loadA => c_loadA,
+    loadD => c_loadD,
+    loadM => writeM,
+    loadPC => c_loadPC
+  );
+
+  MUXALU_I: Mux16 port map(
+    a => s_ALUout
+    b => instruction(15 downto 0),
+    sel => c_muxALUI_A,
+    q => s_muxALUI_Aout
+  );
+
+  REG_A: Register16 port map(
+    clock => clock,
+    input => s_muxALUI_Aout,
+    load => c_loadA,
+    output => s_regAout
+  );
+
+  MUX_AM: Mux16 port map(
+    a => s_regAout,
+    b => inM,
+    sel => c_muxAM,
+    q => s_muxAM_out
+  );
+
+  REG_D: Register16 port map(
+    clock => clock,
+    input => s_ALUout,
+    load => c_loadD,
+    output => s_regDout
+  );
+
+  ALU: ALU port map(
+    x => s_regDout,
+    y => s_muxAM_out,
+    zx => c_zx,
+    nx => c_nx,
+    zy => c_zy,
+    ny => c_ny,
+    f => c_f,
+    no => c_no,
+    zr => c_zr,
+    ng => c_ng,
+    saida => s_ALUout
+  );
+
+  PC: pc port map(
+    clock => clock,
+    increment => '1',
+    load => c_loadPC,
+    reset => reset,
+    input => s_regAout,
+    output => s_pcout
+  );
+  
+  outM <= s_ALUout;
+  addressM <= s_regAout (14 downto 0);
+  pcout <= s_pcout (14 downto 0);
+
+
+
+  
 
 end architecture;
