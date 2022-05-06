0af901aeba4d62289ec3d505d00a13a6b1586312
rafapaolino <paulohenriquemoreirafalcao@gmail.com>
2022-05-06 09:31:24 -0300

    cpu ainda nao testada

--- Projetos/F-Computador/src/CPU.vhd
+++ Projetos/F-Computador/src/CPU.vhd
@@ -100,4 +100,75 @@ architecture arch of CPU is
 
 begin
 
+Alu: ALU port map(
+  x => s_regDout,
+  y => s_muxAM_out,
+  zx => c_zx,
+  nx = c_nx,
+  zy => c_zy,
+  ny => c_ny,
+  f => c_f,
+  no => c_no,
+  zr => c_zr,
+  ng => c_nr,
+  saida => s_ALUout
+);
+
+regD: Register16 port map(
+  clock => clock,
+  input => s_ALUout,
+  load => c_loadD, 
+  output => s_regDout
+);
+
+regA: Register16 port map(
+  clock => clock,
+  input => s_muxALUI_Aout,
+  load => c_loadA,
+  output => s_regAout
+);
+
+muxamd: Mux16 port map(
+  a => s_regAout,
+  b => inM,
+  sel => c_muxAM,
+  q => s_muxAM_out
+);
+
+muxALU: Mux16 port map(
+  a => s_ALUout,
+  b => instruction(15 downto 0),
+  sel => c_muxALUI_A,
+  q => s_muxALUI_Aout
+);
+
+Cu: ControUnit port map(
+  instruction => intruction,
+  zr => c_zr,
+  ng => c_ng,
+  muxALUI_A => c_muxALUI_A,
+  muxAM => c_muxAM,
+  zx => c_zx,
+  nx => c_nx,
+  zy => c_zy,
+  ny => c_ny,
+  f => c_f,
+  no => c_no,
+  loadA => c_loadA,
+  loadD => c_loadD,
+  loadM => writeM,
+  loadPC => c_loadPC
+);
+
+programCounter: PC port map(
+  clock => clock,
+  increment => '1',
+  load => c_loadPC,
+  reset => reset,
+  input => s_regAout,
+  output => s_pcout
+);
+
+outM <= s_ALUout;
+addressM <= s_regAout;
 end architecture;
