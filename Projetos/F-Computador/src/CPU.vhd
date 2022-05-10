0c4ca53960f17d2ed746f1a01707526cf05ff9aa
DomRuanSuzano <dom.ruan.drs@gmail.com>
2022-05-10 20:31:46 -0300

    comeco da implementacao do CPU

--- Projetos/F-Computador/src/CPU.vhd
+++ Projetos/F-Computador/src/CPU.vhd
@@ -100,4 +100,23 @@ architecture arch of CPU is
 
 begin
 
+u0: ControlUnit port map(instruction, c_zr, c_ng, c_muxALUI_A, c_muxAM, c_zx, c_nx, c_zy, c_ny, c_f, c_no, c_loadA, c_loadD, writeM, c_loadPC);
+
+u1: ALU port map(s_regDout, s_muxAM_out, c_zx, c_nx, c_zy, c_ny, c_f, c_no, c_zr, c_ng, s_ALUout);
+
+u2: Mux16 port map(s_ALUout, instruction(15 downto 0), c_muxALUI_A, s_muxALUI_Aout);
+
+u3: Register16 port map(clock, s_muxALUI_Aout, c_loadA, s_regAout);
+
+u4: Mux16 port map(s_regAout, inM, c_muxAM, s_muxALUI_Aout);
+
+u5: Register16 port map(clock, s_ALUout, c_loadD, s_regDout);
+
+u6: pc port map(clock, increment, loadPC, reset, s_regAout, s_pcout);
+
+writeM <= writeM;
+outM <= s_ALUout;
+addressM <= s_regAout(14 downto 0);
+pcout < s_pcout(14 downto 0);
+
 end architecture;
