6c258ebe2f492c34253fe14d17d21112f572e47b
caiotravain <caiotravain@gmail.com>
2022-05-11 15:30:48 -0300

    C

--- Projetos/F-Computador/src/ControlUnit.vhd
+++ Projetos/F-Computador/src/ControlUnit.vhd
@@ -27,8 +27,23 @@ entity ControlUnit is
 end entity;
 
 architecture arch of ControlUnit is
-
 begin
+
+loadD <= '1' when (instruction(17) and instruction(4)) = '1' else '0';
+loadA <= '1' when((not(instruction(17))) or instruction(3)) = '1' else '0';
+loadM <= '1' when (instruction(17) and instruction(5)) = '1' else '0';
+loadPC <= '1' when ((instruction(17) and instruction(1) and zr and (not(ng))) or ((instruction(0) and instruction(17) and not(ng) and not(zr)) or ((instruction(2) and instruction(17) and ng and (not(zr)))))) = '1' else '0';
+
+zx<='1' when (instruction(12) and instruction(17)) = '1' else '0';
+nx<= '1'when (instruction(11) and instruction(17)) = '1' else '0';
+zy<='1' when (instruction(10) and instruction(17)) = '1' else '0';
+ny<= '1'when (instruction(9) and instruction(17)) = '1' else '0';
+f<= '1'when (instruction(8) and instruction(17)) = '1' else '0';
+no<= '1'when (instruction(7) and instruction(17)) = '1' else '0';
+
+muxALUI_A <= '1' when ((instruction(14) and instruction(17)) or (not(instruction(17)))) = '1' else '0';
+muxAM<= '1'when (instruction(13))  = '1' else '0';
+
 
 
 end architecture;
