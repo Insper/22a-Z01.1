a28aaa42ac3a6cf5e286624d07d7233b7729e3a0
Pedro Fracassi <pedroluizfracassi@gmail.com>
2022-05-24 19:17:03 -0300

    Implementa jump

--- Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
+++ Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
@@ -36,8 +36,24 @@ public class Code {
      * @return Opcode (String de 3 bits) com código em linguagem de máquina para a instrução.
      */
     public static String jump(String[] mnemnonic) {
-        /* TODO: implementar */
-    	return "";
+        switch (mnemnonic[0]) {
+            case "jmp":
+                return "111";
+            case "je":
+                return "010";
+            case "jne":
+                return "101";
+            case "jg":
+                return "001";
+            case "jge":
+                return "011";
+            case "jl":
+                return "100";
+            case "jle":
+                return "110";
+            default:
+                return "000";
+        }
     }
 
     /**
