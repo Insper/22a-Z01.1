31c9defaabf464e52fdae842baa88df2bba6fb2f
Arthur-Barreto <arthurbarreto2905@gmail.com>
2022-05-25 15:19:01 -0300

    jump e destiny funcionando

--- Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
+++ Projetos/G-Assembler/Assembler/src/main/java/assembler/Code.java
@@ -5,6 +5,9 @@
 
 package assembler;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.lang.*;
 /**
  * Traduz mnemônicos da linguagem assembly para códigos binários da arquitetura Z0.
  */
@@ -17,7 +20,56 @@ public class Code {
      */
     public static String dest(String[] mnemnonic) {
         /* TODO: implementar */
-    	return "";
+
+        Map <String, Integer> destine = new HashMap<>();
+        destine.put("%A"  , 1);
+        destine.put("%D"  , 10);
+        destine.put("(%A)", 100);
+
+        int finalDestiny = 0;
+        int concatenaveis;
+        String aux;
+        switch (mnemnonic[0]){
+
+            case "movw" : 
+                for (int i=2; i<mnemnonic.length;i++){
+                    finalDestiny += destine.get(mnemnonic[i]);
+                }
+                aux = String.valueOf(finalDestiny);
+                concatenaveis = 4 - aux.length();
+                for (int i = 0; i < concatenaveis; i++) {
+                    aux = '0' + aux;
+                }
+                return aux;
+            case "addw" :
+            case "subw" :
+            case "rsubw" :
+            case "andw" : 
+            case "orw"  : 
+                for (int i=3; i<mnemnonic.length;i++){
+                    finalDestiny += destine.get(mnemnonic[i]);
+                }
+                aux = String.valueOf(finalDestiny);
+                concatenaveis = 4 - aux.length();
+                for (int i = 0; i < concatenaveis; i++) {
+                    aux = '0' + aux;
+                }
+                return aux;
+            case "incw" : 
+            case "decw" :
+            case "notw" :
+            case "negw" :
+                for (int i=1; i<mnemnonic.length;i++){
+                    finalDestiny += destine.get(mnemnonic[i]);
+                }
+                aux = String.valueOf(finalDestiny);
+                concatenaveis = 4 - aux.length();
+                for (int i = 0; i < concatenaveis; i++) {
+                    aux = '0' + aux;
+                }
+                return aux;
+            default     : return "0000";
+        }
     }
 
     /**
@@ -37,7 +89,16 @@ public class Code {
      */
     public static String jump(String[] mnemnonic) {
         /* TODO: implementar */
-    	return "";
+        switch (mnemnonic[0]){
+            case "jg"  : return "001";
+            case "je"  : return "010";
+            case "jge" : return "011";
+            case "jl"  : return "100";
+            case "jne" : return "101";
+            case "jle" : return "110";
+            case "jmp" : return "111";
+            default    : return "000";
+        }
     }
 
     /**
